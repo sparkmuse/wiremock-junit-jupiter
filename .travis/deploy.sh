5ceee33efcc5743f906f75f6af8a59e1aad78f0b
Sparkmuse <sparkmuse.tech@gmail.com>
2020-06-30 22:04:19 +0200

    Add deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 # Update the version
-#mvn versions:set -DnewVersion=1.1.$(git rev-list HEAD --count)
+mvn versions:set -DnewVersion=1.1.$(git rev-list HEAD --count)
 
 # Deploy
-#mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U -Prelease,ossrh
+mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U -Prelease,ossrh
